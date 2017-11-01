991d0ab65fbe8b6a38337169f62bb74ec42b72e8
Fanniepihl <32300785+Fanniepihl@users.noreply.github.com>
2017-11-01 16:51:23 +0100

    steak fish pasta

--- categories.php
+++ categories.php
@@ -1,1 +1,30 @@
 <?php include("header.php") ?>
+<?php include("config.php") ?>
+
+<body>
+
+<div class="container">
+  <img img src="img/fisk.jpeg" class="image">
+  <div class="overlay">
+    <div class="text"><a href="fish.php">Fish</a></div>
+  </div>
+</div><!--end container-->
+
+<div class="container">
+  <img img src="img/stek.jpeg" class="image">
+  <div class="overlay">
+    <div class="text"><a href="steak.php">Steak</a></div>
+  </div>
+</div><!--end container-->
+
+<div class="container">
+  <img img src="img/chick.png" class="image">
+  <div class="overlay">
+    <div class="text"><a href="pasta.php">Pasta</a></div>
+  </div>
+</div><!--end container-->
+
+
+</body>
+
+<?php include("footer.php") ?>
---
+++ steak.php
@@ -0,0 +1,4 @@
+<?php include("header.php") ?>
+<?php include("config.php") ?>
+
+<?php include("footer.php") ?>
