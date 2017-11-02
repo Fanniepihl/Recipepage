f660f4a9c0c29d5aaa23f8a72c9bdd0adece9901
ellinorek <32300795+ellinorek@users.noreply.github.com>
2017-11-02 16:34:52 +0100

    body

--- favourites.php
+++ favourites.php
@@ -1,4 +1,10 @@
-   <?php include("header.php") ?>
-   <?php include("config.php") ?>
+<?php include("header.php") ?>
+<?php include("config.php") ?>
+
+<body>
+
+
+
+</body>
 
 <?php include('footer.php') ?>
