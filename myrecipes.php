c11bcebf6b3fb1c2c4cf2ec03aade07d37eff721
ellinorek <32300795+ellinorek@users.noreply.github.com>
2017-11-08 13:57:29 +0100

    button to log in from my recipe page

--- myrecipes.php
+++ myrecipes.php
@@ -4,6 +4,10 @@
 <body>
 
 <h2>Here is your saved recipes</h2>
+    
+ <h3>To see your recipes you need to logg in</h3>  
+    <button><a href="logIn.php" >Logg in here</a></button>
+    
 <div class="mygallery"><!---en div att lägga in sina uppladdade recept i.--->
     
 </div>
