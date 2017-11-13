bd5c6b20b1a68294411c5f94c4eb223c48bee50c
ellinorek <32300795+ellinorek@users.noreply.github.com>
2017-11-13 14:00:34 +0100

    blä

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <div id="bread"> 
   <h1 class="bread-titel">bread.</h1>
     <div id="start_text"><h2>Welcome to bread. <br>
-Here you can easily find fine recipes for all kinds of tastes. <br>
+Here you can easily find fine recipes for all kinds of tastes.
         Save your favorites from all different kinds of recipes and upload your own recipe for other people to find. 
 </h2>
  </div><!--end start_text-->
