4c9dcff03dc8194e51a172735da77f6225a63c82
nhungvus <nhung@live.se>
2017-11-17 11:03:15 +0100

    Ingredienser även i myrecipes

--- myrecipes.php
+++ myrecipes.php
@@ -250,25 +250,10 @@ foreach ($ingredientsids as $index => $id) {
 		    exit();
 		}
 
-
-		$query = " SELECT recipeid, title, ingredients, description, onloan, image FROM recipe WHERE onloan = 1";
-			if ($searchtitle && !$searchingredients) { // Title search only
-			$query = $query . " Where title like '%" . $searchtitle . "%'";
-				}
-			if (!$searchtitle && $searchingredients) { // Ingredients search only
-			$query = $query . " Where ingredients like '%" . $searchingredients . "%'";
-				}
-			if ($searchtitle && $searchingredients) { // Title and ingredience search
-				$query = $query . " Where title like '%" . $searchtitle . "%' and ingredients like '%" . $searchingredients . "%'"; 
-				} 
-
-		 
-
-		# Here's the query using bound result parameters
-	    // echo "we are now using bound result parameters <br/>";
-	    	$stmt = $db->prepare($query);
-			$stmt->bind_result($recipeid, $title, $ingredients, $description, $onloan, $image);
-			$stmt->execute();
+			$query = "SELECT recipe.recipeid, recipe.title, recipe.description, recipe.onloan, recipe.image, GROUP_CONCAT(ingredients.name) as als FROM recipe, recing, ingredients WHERE onloan = 1 AND recipe.recipeid = recing.recipeid AND recing.ingredientsid = ingredients.ingredientsid GROUP BY recipe.recipeid";
+				$stmt = $db->prepare($query);
+			    $stmt->bind_result($recipeid, $title, $description, $onloan, $image, $grouped_ing);
+			    $stmt->execute();
 
 
 			echo '<table id="t01" style="width:100%" >';
@@ -277,10 +262,11 @@ foreach ($ingredientsids as $index => $id) {
 		        if($onloan==0)
 		            $onloan="No";
 		        else $onloan="Yes";
-		       
+
+		        $grouped_ing = str_replace(",", ", ", $grouped_ing);
 		       
 		        echo "<tr>";
-		        echo "<td> <img src='img/$image' style='max-height:150px;max-width:150px'</img> </td><td> $title </td><td> $ingredients </td> <td> $description </td><td> $onloan </td>";
+		        echo "<td> <img src='img/$image' style='max-height:150px;max-width:150px'</img> </td><td> $title </td><td> $grouped_ing </td> <td> $description </td><td> $onloan </td>";
 		        echo '<td><a href="removerecipe.php?recipeid=' . urlencode($recipeid) . '"><input type="submit" value="Remove"></input></a></td>';
 		        echo "</tr>";
 
