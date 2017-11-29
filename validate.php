efd6dfb0b7a6b2692234906abac7ebbeded25a6a
ellinorek <32300795+ellinorek@users.noreply.github.com>
2017-11-29 14:11:42 +0100

    Kommentarer Validate

--- validate.php
+++ validate.php
@@ -1,19 +1,14 @@
 <?php 
 //$myusername = "ellinor";
-//$myuserpass = "db63cdafaa98b689f15a84df7b00a230730682ed"    
+//$myuserpass = "db63cdafaa98b689f15a84df7b00a230730682ed"  = "ellinor" 
 
-//if(isset($_POST['login'])){
-//   $uname =$_POST['username']; 
-//     $upass =$_POST['userpass'];
-     
-    #if($uname == $myusername and $upass == $myuserpass )
     if(isset($_POST['remember'])){ 
-        setcookie('username',$uname, time()+60*60*7);
+        setcookie('username',$uname, time()+60*60*7);   #om remeber me är i bockad så ska en cookie starta. 
         
         }
-    session_start();
+    session_start();                    #session startas när användarnamnet är rätt. 
     $_SESSION['username'] = $uname;
-    header("location: welcome.php");
+    header("location: welcome.php");      #användaren skickas till welcome.php. 
     
 }
 ?>
